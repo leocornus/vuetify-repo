1f514ea2fa8c3eb878c173609cb946f182f19366
Sean Chen <sean.chen@leocorn.com>
2019-04-26 18:17:55 +0000

    add a simple layout.

--- src/components/vuetify.vue
+++ src/components/vuetify.vue
@@ -34,6 +34,7 @@
 export default {
   name: 'VuetifyScratchPad',
   data () {
+    return {};
   },
 
   methods: {
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import vt from '@/components/vuetify'
+import SimpleLayout from '@/components/start/SimpleLayout'
 
 Vue.use(Router)
 
@@ -16,6 +17,21 @@ export default new Router({
       path: '/vt',
       name: 'vt',
       component: vt
+    },
+
+    {
+        path: '/start',
+        component: SimpleLayout,
+        // set the home page for /start
+        redirect: '/start/vt',
+        // set the children
+        children: [
+            {
+                path: 'vt',
+                name: 'StartVt',
+                component: vt
+            }
+        ]
     }
   ]
 })
