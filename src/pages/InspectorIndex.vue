6de0ccba74833d7aebd6f696507ed8ba71abb7d0
Sean Chen <sean.chen@leocorn.com>
2019-06-18 17:55:40 +0000

    playaround the the v-text-field

--- src/pages/InspectorIndex.vue
+++ src/pages/InspectorIndex.vue
@@ -1,19 +1,20 @@
 <template>
 <div>
-<v-container grid-list-md text-xs-center>
-  <v-layout row wrap>
+<v-container fluid grid-list-md>
+  <v-layout wrap>
     <v-flex xs12>
-      <v-card dark color="red">
-        <v-card-text class="px-0">
-          {{message}}
-        </v-card-text>
-      </v-card>
-    </v-flex>
-
-    <v-flex v-for="i in 12 * 120" :key="`6${i}`" xs1>
-      <v-card dark :color="`#${(i * 50).toString(16)}`"
-        <v-card-text class="px-0">{{i}}</v-card-text>
-      </v-card>
+      <v-text-field
+         v-model="message"
+         append-icon="search"
+         prepend-icon="help"
+         box
+         clear-icon="close"
+         clearable
+         label="What are you looking for?"
+         type="text"
+         @click:clear="clearMessage"
+         @click:append="search"
+      ></v-text-field>
     </v-flex>
   </v-layout>
 </v-container>
@@ -30,6 +31,12 @@ export default {
   },
 
   methods: {
+      clearMessage () {
+          this.message = ''
+      },
+
+      search () {
+      }
   },
   created() {
 
