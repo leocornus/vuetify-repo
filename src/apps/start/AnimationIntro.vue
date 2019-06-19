8781667da9a8098789d2fc7c60b63b4bd249d3b4
Sean Chen <sean.chen@leocorn.com>
2019-06-19 13:04:14 +0000

    set the button to small

--- src/apps/start/AnimationIntro.vue
+++ src/apps/start/AnimationIntro.vue
@@ -3,7 +3,11 @@
   <v-btn v-on:click="add" color="success">Add</v-btn>
   <v-btn v-on:click="remove" color="red">Remove</v-btn>
   <transition-group name="list" tag="p">
-    <v-btn v-for="item in items" v-bind:key="item" class="list-item">
+    <v-btn color="info"
+           small 
+           v-for="item in items" 
+           v-bind:key="item" 
+           class="list-item">
       {{ item }}
     </v-btn>
   </transition-group>
@@ -13,19 +17,25 @@
 <script>
 export default {
   el: '#list-demo',
+
   data() {
+
     return {
-    items: [1,2,3,4,5,6,7,8,9],
-    nextNum: 10
+      items: [1,2,3,4,5,6,7,8,9],
+      nextNum: 10
     };
   },
+
   methods: {
+
     randomIndex: function () {
       return Math.floor(Math.random() * this.items.length)
     },
+
     add: function () {
       this.items.splice(this.randomIndex(), 0, this.nextNum++)
     },
+
     remove: function () {
       this.items.splice(this.randomIndex(), 1)
     },
